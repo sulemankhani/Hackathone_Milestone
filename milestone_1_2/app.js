50c71ad71279f725910ad31b46f1a50117fb3bd3
suleman khan <159639439+sulemankhani@users.noreply.github.com>
2024-09-25 03:27:45 +0500

    Add files via upload

---
+++ milestone_1_2/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleSkillsButton = document.getElementById('toggle-skills');
+    var skillsSection = document.getElementById('skills');
+    if (toggleSkillsButton && skillsSection) {
+        toggleSkillsButton.addEventListener('click', function () {
+            if (skillsSection.style.display === 'none') {
+                skillsSection.style.display = 'block';
+                toggleSkillsButton.textContent = 'Hide Skills';
+            }
+            else {
+                skillsSection.style.display = 'none';
+                toggleSkillsButton.textContent = 'Show Skills';
+            }
+        });
+    }
+});
